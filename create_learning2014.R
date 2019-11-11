22533fca711fdbed1f85ce912dadfe14b8706b1a
Maarit <maarit@91-159-71-108.elisa-laajakaista.fi>
2019-11-11 19:44:36 +0200

    Data wrangling for week 2

---
+++ create_learning2014.R
@@ -0,0 +1,32 @@
+# Maarit Tarkiainen 7 Nov Linear regression exercises
+lrn14<-read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+library(dplyr)
+install.packages("dplyr")
+
+#make vectors of all questions in each dimension
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+#creating temporary columns to include all questions 
+deep_columns <- select(lrn14, one_of(deep_questions))
+surf_columns <- select(lrn14, one_of(surface_questions))
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+
+#creating new columns to the file lrn14
+lrn14$deep <- rowMeans(deep_columns)
+lrn14$surf <- rowMeans(surf_columns)
+lrn14$stra <- rowMeans(strategic_columns)
+
+keep_columns <- c("gender", "Age", "Attitude", "deep", "stra", "surf", "Points")
+learning2014<-select(lrn14, one_of(keep_columns))
+learning2014<- filter (learning2014, Points > 0)
+str(learning2014)
+getwd()  #test which working directory is in use
+setwd("/Users/maarit/IODS-project/data") #setting working directory
+?write.table #to get help
+
+write.table(learning2014, file = "learning2014") #to save the file
+test<-read.table("/Users/maarit/IODS-project/data/learning2014")
+str(test) #testing the structure of the file
